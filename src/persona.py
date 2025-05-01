9da60b95a4ccd680b62c622df1269827d6894581
dana tinnerello <d.tinnerello@alumno.um.edu.ar>
2025-05-01 15:39:10 -0300

    implementacion del codigo del comportamiento de la clase persona

--- src/persona.py
+++ src/persona.py
@@ -1,10 +1,15 @@
-class Persona:
-       def __init__(self, nombre, apellido, dni):
-           self.nombre = nombre
-           self.apellido = apellido
-           self.dni = dni
-           self.pensamientos = 0
-           self.ultima_idea = "<no penso en nada>"
 
-       def __repr__(self):
-           return f"Persona: DNI: {self.dni} Nombre: {self.nombre} Apellido: {self.apellido} Ultima Idea: {self.ultima_idea}"
+class Persona:
+    def __init__(self, nombre, apellido, dni):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.dni = dni
+        self.pensamientos = 0
+        self.ultima_idea = "<no penso en nada>"
+
+    def __repr__(self):
+        return f"Persona: DNI: {self.dni} Nombre: {self.nombre} Apellido: {self.apellido} Ultima Idea: {self.ultima_idea}"
+        
+    def pensar(self, idea):
+        self.pensamientos += 1
+        self.ultima_idea = idea
